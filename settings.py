a1a88900ae7fee050c029abce48aeb2bcf7aa6ac
trinhthaihoang <tth251294@gmail.com>
2023-07-09 19:33:47 +0700

    changed

--- settings.py
+++ settings.py
@@ -1,8 +1,6 @@
 import os
-from dotenv import load_dotenv
 
 # Load các biến môi trường từ tệp .env
-load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
